b0645bbc1f2dd07eaaa21f5e17077db331ddf278
Abhijith Mohanan <abhijithmohanan2002@gmail.com>
2025-07-05 15:23:29 +0530

    Update Exp 2.py

--- Exp 2.py
+++ Exp 2.py
@@ -1,5 +1,5 @@
-print("Niranjana S Nair")
-print("Admission No: A24MCA047")
+print("Name : Abhijith Mohanan ")
+print("Admission No: A24MCA001")
 print("swapped the word Experiment: 2")
 from collections import Counter
 text=input("enter the line of text")
