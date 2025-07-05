069a1422d60cb00085b12e2b47262bac44e1608f
Abhijith Mohanan <abhijithmohanan2002@gmail.com>
2025-07-05 15:22:17 +0530

    Update C-5 Exp 2.py

--- C-5 Exp 2.py
+++ C-5 Exp 2.py
@@ -1,5 +1,5 @@
-print("Name : Niranjana S Nair")
-print("Roll No: 47")
+print("Name : Abhijith Mohanan ")
+print("Admission No: A24MCA001")
 print("Experiment No: 16")
 
 with open("file.txt", "r") as infile, open("output.txt", "w") as outfile:
