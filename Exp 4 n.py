fe5b185d6fe2f1e0689869ed92db169f078dfbe6
Abhijith Mohanan <abhijithmohanan2002@gmail.com>
2025-07-05 15:24:10 +0530

    Update Exp 4 n.py

--- Exp 4 n.py
+++ Exp 4 n.py
@@ -1,6 +1,6 @@
 color_list1=[]
-print("Name: Niranjana Sniar")
-print("Admision No: A24MCA047")
+print("Name : Abhijith Mohanan ")
+print("Admission No: A24MCA001")
 print(" color list Experiment No: 4")
 n=int(input("Enter the limit of colors"))
 print("Enter first color list")
